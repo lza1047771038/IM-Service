507ae21019692c08c6f8a7f0381be14b178a347b
liuzhongao <liuzhongao@corp.netease.com>
2022-12-01 21:10:58 +0800

    code review

--- common/src/main/kotlin/org/im/service/utils/Network.kt
+++ common/src/main/kotlin/org/im/service/utils/Network.kt
@@ -3,7 +3,6 @@ package org.im.service.utils
 import java.net.InetAddress
 import java.net.NetworkInterface
 import java.net.UnknownHostException
-import java.util.*
 
 
 /**
@@ -13,25 +12,20 @@ import java.util.*
 val networkAddress: InetAddress?
     get() = getLocalHostLANAddress()
 
-
 private fun getLocalHostLANAddress(): InetAddress? {
     return try {
         var candidateAddress: InetAddress? = null
-        // 遍历所有的网络接口
-        val ifaces: Enumeration<*> = NetworkInterface.getNetworkInterfaces()
-        while (ifaces.hasMoreElements()) {
-            val iface = ifaces.nextElement() as NetworkInterface
-            // 在所有的接口下再遍历IP
-            val inetAddrs: Enumeration<*> = iface.inetAddresses
-            while (inetAddrs.hasMoreElements()) {
-                val inetAddr = inetAddrs.nextElement() as InetAddress
-                if (!inetAddr.isLoopbackAddress) { // 排除loopback类型地址
-                    if (inetAddr.isSiteLocalAddress) {
-                        // 如果是site-local地址，就是它了
-                        return inetAddr
+        val networkInterfaces = NetworkInterface.getNetworkInterfaces()
+        while (networkInterfaces.hasMoreElements()) {
+            val networkInterface = networkInterfaces.nextElement()
+            val networkAddress = networkInterface.inetAddresses
+            while (networkAddress.hasMoreElements()) {
+                val iNetworkAddress = networkAddress.nextElement()
+                if (!iNetworkAddress.isLoopbackAddress) {
+                    if (iNetworkAddress.isSiteLocalAddress) {
+                        return iNetworkAddress
                     } else if (candidateAddress == null) {
-                        // site-local类型的地址未被发现，先记录候选地址
-                        candidateAddress = inetAddr
+                        candidateAddress = iNetworkAddress
                     }
                 }
             }
@@ -39,7 +33,6 @@ private fun getLocalHostLANAddress(): InetAddress? {
         if (candidateAddress != null) {
             return candidateAddress
         }
-        // 如果没有发现 non-loopback地址.只能用最次选的方案
         val jdkSuppliedAddress = InetAddress.getLocalHost() ?: throw UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.")
         jdkSuppliedAddress
     } catch (e: Exception) {
